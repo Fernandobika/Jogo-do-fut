18b3bf79931fa7043d4d9999d8b163e7f6961725
Fernandobika <alves.rodrigues.fernando@escola.pr.gov.br>
2025-09-29 10:12:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+document.querySelectorAll('nav ul li a').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href').slice(1);
+    cont tergerSelection = document.getElementById(targetId);
 
+    if (targetSelection) {
+      window.scrollTo({
+        top:targetSelection.offsetTop - 60,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
+
+window.addEventListener('scroll', () => {
+  const header = documen.querySelector('header');
+  if(window.scrolly > 50) {
+    header.style.backgoundColor = '#333';
+  } else {
+    header.style.backgroundColor = '#1a1a1a';
+  }
+});
+  
